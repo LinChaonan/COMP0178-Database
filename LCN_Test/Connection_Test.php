24668a8a32b8088ab0b01f68b41ed2fcf8297a10
Chaonan Lin <18252084368@163.com>
2021-11-12 22:05:18 +0000

    Updated Connection_Test.php

--- LCN_Test/Connection_Test.php
+++ LCN_Test/Connection_Test.php
@@ -19,24 +19,4 @@ if (mysqli_query($conn,$sql)){
     echo "Creating Database Failed".mysqli_error($conn)."";
 }
 
-//Creat New Table
-mysqli_select_db($conn,"test");
-
-$table="CREATE TABLE testTable(
-student_id int(11) auto_increment primary key,
-student_no char(10) not null unique,
-student_name char(20) not null)";
-
-if(mysqli_query($conn,$table)){
-
-    echo "Creating Table Successful";
-
-}
-
-else{
-
-    echo "Creating Table Failed".mysqli_error($conn)."";
-
-}
-
 ?>
