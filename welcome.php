d48c7f8e6177d864c8122fd3c47e3b0cab2a8fa0
Chaonan Lin <18252084368@163.com>
2021-11-16 20:53:31 +0000

    added welcome and reset-password pages

--- welcome.php
+++ welcome.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true){
 <body>
 <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
 <p>
-    <a href="reset_password.php" class="btn btn-warning">Reset Your Password</a>
+    <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
     <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
 </p>
 </body>
