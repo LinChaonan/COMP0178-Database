f8d069bdd350708e1b4977bf4d1fef455b09689c
Chaonan Lin <18252084368@163.com>
2021-11-16 21:34:43 +0000

    updated login page

--- login_result.php
+++ login_result.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "config.php";
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
 
@@ -10,9 +11,38 @@ $_SESSION['logged_in'] = true;
 $_SESSION['username'] = "test";
 $_SESSION['account_type'] = "buyer";
 
-echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-// Redirect to index after 5 seconds
-header("refresh:5;url=welcome.php");
+    $email = $_POST["email"];
+    $pwd = $_POST["password"];
+
+    echo $email, $pwd;
+
+    $sql = "SELECT * FROM users WHERE (username='$email') AND (password='$pwd')";
+    //执行上面的sql语句并将结果集赋给result。
+    $result = $link->query($sql);
+    //判断结果集的记录数是否大于0
+    if ($result->num_rows > 0) {
+        echo "matched";
+        $_SESSION['logged_in'] = true;
+        echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+
+        // Redirect to index after 5 seconds
+        header("refresh:5;url=welcome.php");
+        }
+    else {
+    echo "无信息匹配";
+    echo('<div class="text-center">log in failed! You will be redirected shortly.</div>');
+    header("refresh:5;url=logout.php");
+    }
+
+    if (!$link) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    mysqli_close($link);
+
+
+}
 
 ?>
