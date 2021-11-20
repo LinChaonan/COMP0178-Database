26c2ce529d165e8104854a550ba1f231989dc7b8
yitingcao <cao36670696@gmail.com>
2021-11-20 17:02:11 +0000

    chance to register

--- process_registration.php
+++ process_registration.php
@@ -1,24 +1,83 @@
 
 <?php
+
+include_once("register.php");
+require_once "config.php";
+// TODO: Extract $_POST variables, check they're OK, and attempt to login.
+// Notify user of success/failure and redirect/give navigation options.
+
+// For now, I will just set session variables and redirect.
+
 session_start();
 
+//$_SESSION['account_type'] = "buyer";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $email = $_POST["email"];
+    $pwd = $_POST["password"];
+
+    echo $email, $pwd;
+
+    if (!$link) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO user (email, password) 
+  			  VALUES('$email', '$pwd')";
+
+}
+
+/*
+require_once "config.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
+
+    $email= $password = $passwordConfirmation = "";
+    $email_err = $password_err = $passwordConfirmation_err = "";
+
+    if (empty($_POST["email"])) {
+        $email_err = "Email is required";
+    } else {
+        $email = trim($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_err = "Invalid email format";
+        }
+    }
+
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have at least 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+
+    if(empty(trim($_POST["repeat_password"]))){
+        $passwordConfirmation_err = "Please confirm password.";
+    } else {
+        $passwordConfirmation = trim($_POST["repeat_password"]);
+        if (empty($password_err) && ($password != $passwordConfirmation)) {
+            $passwordConfirmation_err = "Password did not match.";
+        }
+    }
+
+
+
     $email = trim($_POST["email"]);
     $password = trim($_POST["password"]);
+    echo $email, $password;
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "test";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    $sql = "INSERT INTO users (email, password) 
+    $sql = "INSERT INTO user (email, password) 
   			  VALUES('$email', '$password')";
-
 
     $_SESSION['email'] = $email;
     $_SESSION['success'] = "You are now logged in";
     header('location: browse.php');
+
 }
+
+*/
