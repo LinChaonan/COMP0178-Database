9cda5627e6575e7ab206635896218c2c1977355b
Chaonan Lin <18252084368@163.com>
2021-11-25 16:48:28 +0000

    25 Nov

--- auction_system_25_Nov.sql
+++ auction_system_25_Nov.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost:8889
--- 生成日期： 2021-11-21 14:27:19
+-- 生成日期： 2021-11-25 16:47:26
 -- 服务器版本： 5.7.34
 -- PHP 版本： 7.4.21
 
@@ -98,7 +98,19 @@ INSERT INTO `item` (`item_id`, `seller_id`, `category`, `status`, `title`, `desc
 (2, 1, 'computer', '0', 'MacBook', 'MacBook Pro 2021', 1, '500', '40', '350', 5, '400', 2, '2021-11-20 16:39:27.000000'),
 (3, 1, 'tablet', '0', 'iPad', '11th Gen', 1, '300', '30', '250', 8, '300', 2, '2021-11-20 16:39:29.000000'),
 (4, 1, 'earphone', '0', 'AirPods', 'AirPods Pro', 1, '199', '80', NULL, 0, NULL, NULL, '2021-11-19 16:55:00.000000'),
-(5, 1, 'earphone', '0', 'X Display', 'X Pro Display', 1, '10000', '500', NULL, 0, NULL, NULL, '2021-11-20 17:07:00.000000');
+(5, 1, 'earphone', '0', 'X Display', 'X Pro Display', 1, '10000', '500', NULL, 0, NULL, NULL, '2021-11-20 17:07:00.000000'),
+(6, 1, 'earphone', '0', 'Apple Watch', 'Apple Watch Series 7', 1, '369', '300', NULL, 0, NULL, NULL, '2021-11-21 19:36:00.000000'),
+(7, 1, 'earphone', '0', 'HomePod Mini', 'HomePod mini, colour: yellow.', 1, '80', '50', NULL, 0, NULL, NULL, '2021-11-21 19:37:00.000000'),
+(8, 1, 'earphone', '0', 'MacBook Air', 'MacBook Air 2020', 1, '900', '500', NULL, 0, NULL, NULL, '2021-11-20 19:38:00.000000'),
+(9, 1, 'earphone', '0', 'Mac Mini', 'Mac mini M1', 1, '699', '600', NULL, 0, NULL, NULL, '2021-11-21 19:38:00.000000'),
+(10, 1, 'earphone', '0', 'Polishing Cloth', '', 1, '19', '5', NULL, 0, NULL, NULL, '2021-11-12 15:29:00.000000'),
+(11, 1, 'earphone', '0', 'AirTag', '', 1, '29', '10', NULL, 0, NULL, NULL, '2021-11-05 15:29:00.000000'),
+(12, 1, 'earphone', '0', 'Beats Flex', 'All Day Wireless Earphones', 1, '59', '20', NULL, 0, NULL, NULL, '2021-11-19 15:32:00.000000'),
+(13, 1, 'earphone', '0', 'YITING CAO', 'Come from China', 1, '200', '50', NULL, 0, NULL, NULL, '2021-11-28 15:18:00.000000'),
+(14, 1, 'earphone', '0', 'YY', '', 1, '', '', NULL, 0, NULL, NULL, '2021-11-05 15:43:00.000000'),
+(15, 1, 'earphone', '0', 'ZZ', 'ZZZ', 1, '20', '200', NULL, 0, NULL, NULL, '2021-11-12 15:59:00.000000'),
+(16, 5, 'earphone', '0', 'MM', 'as', 1, '20', '200', NULL, 0, NULL, NULL, '2021-11-12 15:59:00.000000'),
+(17, 5, 'earphone', '0', 'KK', 'ss', 1, '20', '200', NULL, 0, NULL, NULL, '2021-11-10 16:14:00.000000');
 
 -- --------------------------------------------------------
 
@@ -124,7 +136,7 @@ CREATE TABLE `user` (
   `bank_detail` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `phone` varchar(100) DEFAULT NULL,
-  `account_type` tinyint(1) NOT NULL
+  `account_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -132,8 +144,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `email`, `password`, `bank_detail`, `address`, `phone`, `account_type`) VALUES
-(1, 'seller1@gmail.com', '12345678', NULL, NULL, NULL, 0),
-(2, 'buyer1@gmail.com', '12345678', NULL, NULL, NULL, 1);
+(2, 'buyer1@gmail.com', '12345678', NULL, NULL, NULL, 'buyer'),
+(5, 'seller1@gmail.com', '12345678', NULL, NULL, NULL, 'seller');
 
 --
 -- 转储表的索引
@@ -202,7 +214,7 @@ ALTER TABLE `historical_auction_price`
 -- 使用表AUTO_INCREMENT `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `item_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- 使用表AUTO_INCREMENT `seller`
@@ -214,7 +226,7 @@ ALTER TABLE `seller`
 -- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
