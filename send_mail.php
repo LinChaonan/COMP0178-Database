79f0f276768ed2ae82784ad580b38e44bdf956a2
Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
2021-12-02 20:26:42 +0000

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -17,7 +17,6 @@ function send_email($email, $subject, $body)
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
