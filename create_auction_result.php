c0ec91f4ea604baccb05a82b532f88449a037fe9
Chaonan Lin <18252084368@163.com>
2021-12-02 17:34:57 +0000

    2 Dec

--- create_auction_result.php
+++ create_auction_result.php
@@ -60,10 +60,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         ///
         //1.获取上传文件信息
         $upfile=$_FILES["pic"];
-    //定义允许的类型
+        //定义允许的类型
         $typelist=array("image/jpeg","image/jpg","image/png","image/gif");
         $path="./images/";//定义一个上传后的目录
-    //2.过滤上传文件的错误号
+        //2.过滤上传文件的错误号
         if($upfile["error"]>0){
             switch($upfile['error']){//获取错误信息
                 case 1:
@@ -85,21 +85,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     $info="文件写入失败！";break;
             }die("上传文件错误,原因:".$info);
         }
-    //3.本次上传文件大小的过滤（自己选择）
+        //3.本次上传文件大小的过滤（自己选择）
         if($upfile['size']>10000000){
             die("上传文件大小超出限制");
         }
-    //4.类型过滤
+        //4.类型过滤
         if(!in_array($upfile["type"],$typelist)){
             die("上传文件类型非法!".$upfile["type"]);
         }
-    //5.上传后的文件名定义(随机获取一个文件名)
+        //5.上传后的文件名定义(随机获取一个文件名)
         $fileinfo=pathinfo($upfile["name"]);//解析上传文件名字
         do{
             $newfile=date("YmdHis").rand(1000,9999).".".$fileinfo["extension"];
         }while(file_exists($path.$newfile));
-    //6.执行文件上传
-    //判断是否是一个上传的文件
+        //6.执行文件上传
+        //判断是否是一个上传的文件
         if(is_uploaded_file($upfile["tmp_name"])){
             //执行文件上传(移动上传文件)
             if(move_uploaded_file($upfile["tmp_name"],$path.$newfile)){
@@ -113,7 +113,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 $pass = 'root'; //对应的密码
                 $dsn ="mysql:host = $host;dbname=$dbName";
                 $pdo = new PDO($dsn,$user,$pass);
-                $data = addslashes(fread(fopen($pic,"r"),filesize($pic)));
+//                $data = addslashes(fread(fopen($pic,"r"),filesize($pic)));
                 //将图片的名称和路径存入数据库
                 $query = "INSERT INTO image(name,path)VALUES('$newfile','$path$newfile')";
                 $result = $pdo -> query($query);
