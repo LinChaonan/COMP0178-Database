86740e5759c49463fcf3c62f0d8d021f548c7d83
Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
2021-12-06 16:06:23 +0000

    Add script in the login_result

--- login_result.php
+++ login_result.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "config.php";
+require_once "send_mail.php";
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
 
@@ -13,29 +14,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $_POST["email"];
     $pwd = $_POST["password"];
 
-    echo $email, $pwd;
 
     $sql = "SELECT * FROM user WHERE (email='$email') AND (password='$pwd')";
-    //执行上面的sql语句并将结果集赋给result。
     $result = $link->query($sql);
-    //判断结果集的记录数是否大于0
     if ($result->num_rows > 0) {
-        echo "matched";
         $row = $result->fetch_assoc();
         $accountType = $row["account_type"];
         $_SESSION['logged_in'] = true;
         $_SESSION['userID'] = $row["user_id"];
         $_SESSION['account_type'] = $accountType;
-        echo $_SESSION['userID'];
-        echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+        $_SESSION['email'] = $email;
+        echo('<script>alert("You are now logged in! You will be redirected shortly.")</script>');
 
         // Redirect to index after 5 seconds
-        header("refresh:5;url=browse.php");
+        header("refresh:0;url=browse.php");
         }
     else {
-    echo "无信息匹配";
-    echo('<div class="text-center">log in failed! You will be redirected shortly.</div>');
-    header("refresh:5;url=logout.php");
+    echo('<script>alert("Log in failed! Email or password does not match.")</script>');
+    header("refresh:0;url=logout.php");
     }
 
     if (!$link) {
