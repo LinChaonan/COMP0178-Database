3d040bb484da89e79cb86db2db1f2966a2b46410
Chaonan Lin <18252084368@163.com>
2021-12-19 15:31:20 +0000

    Final SQL Script

--- auction_system.sql
+++ auction_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 18, 2021 at 10:45 PM
+-- Generation Time: Dec 19, 2021 at 03:17 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `auction_system`
 --
+CREATE DATABASE IF NOT EXISTS `auction_system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `auction_system`;
 
 -- --------------------------------------------------------
 
